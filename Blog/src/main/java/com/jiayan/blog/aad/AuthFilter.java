ff91ab932d1ff17827359113901bf09c982d2834
jiayanfighting <jiayanfighting@163.com>
2020-08-01 17:18:46 +0800

    22

--- Blog/src/main/java/com/jiayan/blog/aad/AuthFilter.java
+++ Blog/src/main/java/com/jiayan/blog/aad/AuthFilter.java
@@ -35,8 +35,11 @@ public class AuthFilter implements Filter{
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
                          FilterChain chain) throws IOException, ServletException {
-
-        if (request instanceof HttpServletRequest) {
+        if (request instanceof HttpServletRequest){
+            chain.doFilter(request, response);
+            return;
+        }
+        if (!(request instanceof HttpServletRequest)) {
             HttpServletRequest httpRequest = (HttpServletRequest) request;
             HttpServletResponse httpResponse = (HttpServletResponse) response;
             try {
